3988a00f18ab4c074cd8e58bb61da55ab908ac5c
SJTU-Lucy <frenkiedejong@sjtu.edu.cn>
2021-09-17 10:33:48 +0800

    delete unnecessary files

---
+++ DisJointedSet.cpp
@@ -0,0 +1,100 @@
+﻿#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<stack>
+#include<queue>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+
+class DisjointedSet
+{
+public:
+	int* parent;  //若为根节点，保存的负值为元素个数，若为正则为父亲
+	int size;
+	//初始化，一开始的值全为-1
+	DisjointedSet(int n)
+	{
+		size = n + 1;
+		parent = new int[size];
+		for (int i = 0; i < size; ++i)
+			parent[i] = -1;
+	}
+	~DisjointedSet()
+	{
+		delete[] parent;
+	}
+	void print()
+	{
+		for (int i = 1; i < size; ++i)
+			cout << Find(i) << " ";
+		cout << endl;
+	}
+	void Union(int x, int y)
+	{
+		int root1 = Find(x);
+		int root2 = Find(y);
+		if (root1 == root2) return;		
+		if (parent[root1] > parent[root2]) 
+		{
+			parent[root2] += parent[root1];
+			parent[root1] = root2;
+		} 
+		else
+		{
+			parent[root1] += parent[root2];
+			parent[root2] = root1;
+		}
+	}
+	int Find(int x)
+	{
+		if (parent[x] < 0) return x;
+		int myStack[100];
+		int head = 0;
+		int current = x;      //是最终的答案
+		int tmp;
+		while (true)
+		{
+			//current到达根节点
+			if (parent[current] < 0)
+				break;
+			else
+			{
+				myStack[head++] = current;
+				current = parent[current];
+			}
+		}
+		head -= 1;
+		while (head >= 0)
+		{
+			tmp = myStack[head--];
+			parent[tmp] = current;
+		}
+		return current;
+	}
+};
+
+int main()
+{
+	int m, n, p;
+	cin >> m >> n >> p;
+	DisjointedSet mySet(m);
+	int order1, order2;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> order1 >> order2;
+		mySet.Union(order1, order2);
+	}
+	for (int i = 0; i < p; ++i)
+	{
+		cin >> order1 >> order2;
+		if(mySet.Find(order1) == mySet.Find(order2))
+			cout << "Yes" << '\n';
+		else cout << "No" << '\n';
+	}
+	return 0;
+	
+}
---
+++ Heap.cpp
@@ -0,0 +1,199 @@
+﻿#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+template<typename T>
+class Heap_min
+{
+private:
+    int capacity;
+    T* heap;
+    int len;
+public:
+    Heap_min(int x)
+    {
+        capacity = x;
+        heap = new T[capacity];
+        len = 0;
+    }
+    ~Heap_min() 
+    {
+        delete[]heap; 
+    }
+    //打印节点的元素
+    void print()
+    {
+        cout << "the current heap elements are:" << endl;
+        for (int i = 1; i <= len; ++i)
+            cout << heap[i] << " ";
+        cout << endl;
+    }
+    //向下移动节点
+    void minheapify(int x)
+    {
+        int smallest = x;
+        int l, r;
+        T tmp;
+        while (true)
+        {
+            l = x << 1;
+            r = l + 1;
+            if (l <= len && heap[l] < heap[x]) smallest = l;
+            if (r <= len && heap[r] < heap[smallest]) smallest = r;
+            if (smallest != x)
+            {
+                tmp = heap[smallest];
+                heap[smallest] = heap[x];
+                heap[x] = tmp;
+                x = smallest;
+            }
+            else break;
+        }
+    }
+    //上浮
+    void goup(int x)
+    {
+        int i = x;
+        T tmp;
+        while (i > 1 && heap[i / 2] > heap[i])
+        {
+            tmp = heap[i / 2];
+            heap[i / 2] = heap[i];
+            heap[i] = tmp;
+            i = i / 2;
+        }
+    }
+    //插入节点元素(即入队)
+    void insert(T x)
+    {
+        heap[++len] = x;
+        goup(len);
+    }
+    //弹出
+    T pop()
+    {
+        T ret = heap[1];
+        heap[1] = heap[len--];
+        minheapify(1);
+        return ret;
+    }
+    T Min()
+    {
+        return heap[1];
+    }
+};
+
+template<typename T>
+class Heap_max
+{
+private:
+    T* heap;
+    int len;
+public:
+    Heap_max(int x)
+    {
+        heap = new T[x];
+        len = 0;
+    }
+    ~Heap_max()
+    {
+        delete[]heap;
+    }
+    //打印节点的元素
+    void print()
+    {
+        cout << "the current heap elements are:" << endl;
+        for (int i = 1; i <= len; ++i)
+            cout << heap[i] << " ";
+        cout << endl;
+    }
+    //向下移动节点
+    void maxheapify(int x)
+    {
+        int biggest = x;
+        int l, r;
+        T tmp;
+        while (true)
+        {
+            l = x << 1;
+            r = l + 1;
+            if (l <= len && heap[l] > heap[x]) biggest = l;
+            if (r <= len && heap[r] > heap[biggest]) biggest = r;
+            if (biggest != x)
+            {
+                tmp = heap[biggest];
+                heap[biggest] = heap[x];
+                heap[x] = tmp;
+                x = biggest;
+            }
+            else break;
+        }
+    }
+    //上浮
+    void goup(int x)
+    {
+        int i = x;
+        T tmp;
+        while (i > 1 && heap[i / 2] < heap[i])
+        {
+            tmp = heap[i / 2];
+            heap[i / 2] = heap[i];
+            heap[i] = tmp;
+            i = i / 2;
+        }
+    }
+    //插入节点元素(即入队)
+    void insert(int x)
+    {
+        heap[++len] = x;
+        goup(len);
+    }
+    //弹出
+    T pop()
+    {
+        T ret = heap[1];
+        heap[1] = heap[len--];
+        maxheapify(1);
+        return ret;
+    }
+    T Max()
+    {
+        return heap[1];
+    }
+};
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    Heap_max<int> myHeap(100);
+    myHeap.insert(10);
+    myHeap.insert(5);
+    myHeap.insert(15);
+    cout << myHeap.pop() << endl;
+    cout << myHeap.pop() << endl;
+    cout << myHeap.pop() << endl;
+ 
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
